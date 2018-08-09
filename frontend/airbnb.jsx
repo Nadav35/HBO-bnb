60a1c7f1c3bcb389410db2f639b75630cdf827ff
Nadav <nadavnoy@yahoo.com>
2018-08-09 08:33:28 -0700

    Added preLoadedState functionality

--- frontend/airbnb.jsx
+++ frontend/airbnb.jsx
@@ -6,11 +6,23 @@ import { login, signup, logout } from './actions/session_actions';
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById('root');
-  const store = configureStore();
+  let store;
+  if (window.currentUser) {
+    const preloadedState = {
+      entities: {
+        users: { [window.currentUser.id]: window.currentUser}
+      },
+      session: { currentUser: window.currentUser }
+    };
+    store = configureStore(preloadedState);
+    delete window.currentUser;
+  } else {
+    store = configureStore();
+  }
   window.store = store;
-  window.login = login;
-  window.signup = signup;
-  window.logout = logout;
+  // window.login = login;
+  // window.signup = signup;
+  // window.logout = logout;
   ReactDOM.render(<Root store={ store }/>, root);
 
 });
