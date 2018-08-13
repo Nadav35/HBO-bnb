875026d26507e8d50ed2b89381b140283d2afea0
App Academy Student <appacademy@Apps-Mac-mini-5.local>
2018-08-12 19:45:55 -0700

    forgot to save a file

---
+++ frontend/components/spot_show/edit_booking_form.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+import merge from 'lodash/merge';
+
+class EditBookingForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: '',
+      description: '',
+      lng: '',
+      lat: '',
+      location: '',
+      price: '',
+      photoFile: null,
+      photoUrl: null
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleFile = this.handleFile.bind(this);
+  }
+
+  update(property) {
+    return e => this.setState({ [property]: e.target.value });
+  }
+
+  componentDidMount(){
+    this.props.clearErrors();
+  }
+}
